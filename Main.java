92ba30fd291ec7b2da8338cb3edec426fe5fabe1
justinavu <justinvu2008@gmail.com>
2024-11-12 10:16:57 -0800

    wah

--- Main.java
+++ Main.java
@@ -4,14 +4,35 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    p2("Peter Piper picked a pack of pickle peppers.");
+    //p1("Peter Piper picked a pack of pickle peppers.");
   }
 
-  public static boolean isPrime(int N)
+  public static void p2(String a)
   {
-    // TODO: Replace and write your code below
-    return false;
+    String letters = "etaio";
+    String ans = "";
+    for (int i = 0; i < a.length(); i++)
+    {
+      if (!letters.contains(a.substring(i, i+1)))
+      {
+        ans += a.substring(i, i+1);
+      }
+    }
+    System.out.println(ans);
+  }
+  public static void p1(String a)
+  {
+  int count = 0;
+  for (int i = 0; i < a.length()-1; i++)
+  {
+  String pair = a.substring(i, i+2);
+  pair = pair.toLowerCase();
+  if (pair.equals("pa") || pair.equals("pe") || pair.equals("pi") || pair.equals("po") || pair.equals("pu"))
+  {
+    count++;
+  }
+  }
+  System.out.println("Contains p followed by a vowel " + count + " times.");
   }
 }
